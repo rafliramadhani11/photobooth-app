d85bdae155c6bddb92142e57361fcdf3def8fafe
Rafli Ramadhani <rafliramadhani438@gmail.com>
2026-09-02 23:50:00 +0700

    vercel setup #2

--- api/index.php
+++ api/index.php
@@ -1,3 +1,20 @@
 <?php
 
+$_ENV['APP_STORAGE'] = '/tmp/storage';
+
+$storagePaths = [
+    '/tmp/storage/logs',
+    '/tmp/storage/framework/cache/data',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/testing',
+    '/tmp/storage/app/public',
+];
+
+foreach ($storagePaths as $path) {
+    if (!is_dir($path)) {
+        mkdir($path, 0775, true);
+    }
+}
+
 require __DIR__ . '/../public/index.php';
